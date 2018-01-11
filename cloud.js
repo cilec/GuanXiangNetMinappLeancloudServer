a1a3a5ed45858821468d5a9b79d9f9b46f149773
cc <19948031@qq.com>
2018-01-11 10:48:59 +0800

    调试获得accessToken

--- cloud.js
+++ cloud.js
@@ -1,11 +1,25 @@
-var AV = require('leanengine');
+var AV = require("leanengine");
 
 /**
  * 一个简单的云代码方法
  */
-AV.Cloud.define('hello', function(request) {
-  return 'Hello world!';
+AV.Cloud.define("hello", function(request) {
+  return "Hello world!";
 });
-AV.Cloud.define('getScanCode',function(request){
-  return 'getScanCode'
-})
+AV.Cloud.define("getScanCode", function(request) {
+  let options = {
+    method: "POST",
+    mode: "cors",
+    cache: "default"
+  };
+  return fetch(
+    `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${
+      process.env.WEIXIN_APPID
+    }&secret=${process.env.WEIXIN_APPSECRET}`,
+    options
+  )
+    .then(res => {
+      return res;
+    })
+    .catch(err => console.log(err));
+});
