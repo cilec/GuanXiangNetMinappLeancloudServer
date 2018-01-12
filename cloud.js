0da4986c8357da99dc31aea6c87588c242ae33a5
cc <19948031@qq.com>
2018-01-12 17:56:30 +0800

    封装request为promise

--- cloud.js
+++ cloud.js
@@ -1,6 +1,7 @@
+
 var AV = require("leanengine");
 let Request = require("request");
-
+let fs = require("fs");
 /**
  * 一个简单的云代码方法
  */
@@ -8,28 +9,74 @@ AV.Cloud.define("hello", function(request) {
   return "Hello world!";
 });
 AV.Cloud.define("getScanCode", function(request) {
-  Request(
-    `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${
-      process.env.WEIXIN_APPID
-    }&secret=${process.env.WEIXIN_APPSECRET}`,
-    (error, response, body) => {
-      if (!error && response.statusCode == 200) {
-        console.log(JSON.parse(body)); // 打印google首页
+  new Promise(function(resolve) {
+    Request(
+      `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${
+        process.env.WEIXIN_APPID
+      }&secret=${process.env.WEIXIN_APPSECRET}`,
+      (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+          let access_token=JSON.parse(body).access_token
+          console.log(access_token)
+          resolve(access_token);
+        } else {
+          throw error;
+        }
+      }
+    );
+  })
+    .then(res => {
+      return new Promise((res, resolve) => {
+        console.log(res)
         Request.post(
-          `https://api.weixin.qq.com/wxa/getwxacode?access_token=${
-            JSON.parse(body).access_token
-          }`,
+          `https://api.weixin.qq.com/wxa/getwxacode?access_token=${res}`,
           {
-            form: JSON.stringify({  //这里是个巨坑，微信不支持表单提交，必须转成json字符串
+            form: JSON.stringify({
+              //这里是个巨坑，微信不支持表单提交，必须转成json字符串
               path: "pages/index/index",
               auto_color: true
             })
           },
           (error, response, body) => {
-            console.log("二维码", body);
+            if (!error && response.statusCode == 200) {
+              resolve(body);
+            } else {
+              throw error;
+            }
           }
         );
-      }
-    }
-  );
+      });
+    })
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => console.log(err));
+  //   Request(
+  //     `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${
+  //       process.env.WEIXIN_APPID
+  //     }&secret=${process.env.WEIXIN_APPSECRET}`,
+  //     (error, response, body) => {
+  //       if (!error && response.statusCode == 200) {
+  //         // console.log("获取access_token", JSON.parse(body));
+  //         return Request.post(
+  //           `https://api.weixin.qq.com/wxa/getwxacode?access_token=${
+  //             JSON.parse(body).access_token
+  //           }`,
+  //           {
+  //             form: JSON.stringify({
+  //               //这里是个巨坑，微信不支持表单提交，必须转成json字符串
+  //               path: "pages/index/index",
+  //               auto_color: true
+  //             })
+  //           },
+  //           (error, response, body) => {
+  //             console.log("二维码");
+  //             base64Img = body.toString("base64");
+  //             console.log("base64Img", base64Img);
+  //           }
+  //         );
+  //       }
+  //     }
+  //   );
+  //   return base64Img;
 });
