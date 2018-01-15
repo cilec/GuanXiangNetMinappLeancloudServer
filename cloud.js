a2486758819be58f7fb694c7889853cea7cad5b2
cc <19948031@qq.com>
2018-01-15 21:14:39 +0800

    getscancode内promise封装，返回base64的二维码图片

--- cloud.js
+++ cloud.js
@@ -1,4 +1,3 @@
-
 var AV = require("leanengine");
 let Request = require("request");
 let fs = require("fs");
@@ -9,15 +8,15 @@ AV.Cloud.define("hello", function(request) {
   return "Hello world!";
 });
 AV.Cloud.define("getScanCode", function(request) {
-  new Promise(function(resolve) {
+  return new Promise(function(resolve) {
     Request(
       `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${
         process.env.WEIXIN_APPID
       }&secret=${process.env.WEIXIN_APPSECRET}`,
       (error, response, body) => {
         if (!error && response.statusCode == 200) {
-          let access_token=JSON.parse(body).access_token
-          console.log(access_token)
+          let access_token = JSON.parse(body).access_token;
+          console.log(`access_token`, access_token);
           resolve(access_token);
         } else {
           throw error;
@@ -26,8 +25,8 @@ AV.Cloud.define("getScanCode", function(request) {
     );
   })
     .then(res => {
-      return new Promise((res, resolve) => {
-        console.log(res)
+      return new Promise(resolve => {
+        console.log(res);
         Request.post(
           `https://api.weixin.qq.com/wxa/getwxacode?access_token=${res}`,
           {
@@ -39,6 +38,9 @@ AV.Cloud.define("getScanCode", function(request) {
           },
           (error, response, body) => {
             if (!error && response.statusCode == 200) {
+              console.log("二维码");
+              base64Img = body.toString("base64");
+              console.log("base64Img", base64Img);
               resolve(body);
             } else {
               throw error;
@@ -49,34 +51,9 @@ AV.Cloud.define("getScanCode", function(request) {
     })
     .then(res => {
       console.log(res);
+      let base64Img = res.toString("base64");
+      console.log("base64Img", base64Img);
+      return base64Img;
     })
     .catch(err => console.log(err));
-  //   Request(
-  //     `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${
-  //       process.env.WEIXIN_APPID
-  //     }&secret=${process.env.WEIXIN_APPSECRET}`,
-  //     (error, response, body) => {
-  //       if (!error && response.statusCode == 200) {
-  //         // console.log("获取access_token", JSON.parse(body));
-  //         return Request.post(
-  //           `https://api.weixin.qq.com/wxa/getwxacode?access_token=${
-  //             JSON.parse(body).access_token
-  //           }`,
-  //           {
-  //             form: JSON.stringify({
-  //               //这里是个巨坑，微信不支持表单提交，必须转成json字符串
-  //               path: "pages/index/index",
-  //               auto_color: true
-  //             })
-  //           },
-  //           (error, response, body) => {
-  //             console.log("二维码");
-  //             base64Img = body.toString("base64");
-  //             console.log("base64Img", base64Img);
-  //           }
-  //         );
-  //       }
-  //     }
-  //   );
-  //   return base64Img;
 });
