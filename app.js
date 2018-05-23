f038933b1b2dab7c874d6f02d2e1376c8f50a3ce
cc <19948031@qq.com>
2018-05-23 21:52:36 +0800

    bug fix

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const views = require('koa-views');
 const statics = require('koa-static');
 const bodyParser = require('koa-bodyparser');
 
-const weixin=require('./routes/weixin')
+const weixin = require('./routes/weixin');
 // 加载云函数定义，你可以将云函数拆分到多个文件方便管理，但需要在主文件中加载它们
 require('./cloud');
 
@@ -28,7 +28,7 @@ app.use(router.routes());
 app.use(AV.koa());
 
 app.use(bodyParser());
-app.use('/weixin',weixin)
+
 router.get('/', async function(ctx) {
   ctx.state.currentTime = new Date();
   await ctx.render('./index.ejs');
@@ -36,5 +36,5 @@ router.get('/', async function(ctx) {
 
 // 可以将一类的路由单独保存在一个文件中
 app.use(require('./routes/todos').routes());
-
+app.use(require('./routes/weixin').routes());
 module.exports = app;
